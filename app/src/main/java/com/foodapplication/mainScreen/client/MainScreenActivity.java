91292198931001711f0bf6ef38f456f81f4aaeab
bijaybogati <bjdayslight@gmail.com>
2017-11-25 15:02:30 +0545

    file and lib update

--- app/src/main/java/com/foodapplication/mainScreen/client/MainScreenActivity.java
+++ app/src/main/java/com/foodapplication/mainScreen/client/MainScreenActivity.java
@@ -1,15 +1,29 @@
 package com.foodapplication.mainScreen.client;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.foodapplication.R;
 
-public class MainScreenActivity extends AppCompatActivity implements MainScreenController.View{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainScreenActivity extends AppCompatActivity implements MainScreenController.View {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tabs)
+    TabLayout tabs;
+    @BindView(R.id.viewpager)
+    ViewPager viewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+        ButterKnife.bind(this);
     }
 }
