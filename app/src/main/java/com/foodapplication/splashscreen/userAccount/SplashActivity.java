5a1194e951a012ba6b77b36a7b1aa4bfb34c913a
Saugat <saugatboii44@gmail.com>
2017-11-26 15:38:08 +0545

    layout_home_feeds changes

--- app/src/main/java/com/foodapplication/splashscreen/userAccount/SplashActivity.java
+++ app/src/main/java/com/foodapplication/splashscreen/userAccount/SplashActivity.java
@@ -10,6 +10,6 @@ public class SplashActivity extends AppCompatActivity implements SplashControlle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.layout_home_feeds);
     }
 }
