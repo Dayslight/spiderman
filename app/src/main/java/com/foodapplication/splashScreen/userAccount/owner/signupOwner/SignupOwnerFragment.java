914db05609e2b0a1a92536daac81cf77dadbc9d0
Your Name <devkkali@gmail.com>
2017-11-27 18:01:23 +0545

    incomplete owner layout

--- app/src/main/java/com/foodapplication/splashScreen/userAccount/owner/signupOwner/SignupOwnerFragment.java
+++ app/src/main/java/com/foodapplication/splashScreen/userAccount/owner/signupOwner/SignupOwnerFragment.java
@@ -1,19 +1,71 @@
 package com.foodapplication.splashScreen.userAccount.owner.signupOwner;
 
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.AppCompatImageView;
+import android.support.v7.widget.AppCompatTextView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.foodapplication.R;
+import com.foodapplication.Utility;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SignupOwnerFragment extends Fragment implements SignupOwnerController.View{
+public class SignupOwnerFragment extends Fragment implements SignupOwnerController.View {
 
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private String userChoosenTask;
+
+
+
+
+    @BindView(R.id.resturantImageView)
+    CircleImageView resturantImageView;
+    @BindView(R.id.txtShortDescription)
+    AppCompatTextView txtShortDescription;
+    @BindView(R.id.edtRestaurantName)
+    TextInputEditText edtRestaurantName;
+    @BindView(R.id.textInputLayoutRestaurantName)
+    TextInputLayout textInputLayoutRestaurantName;
+    @BindView(R.id.edtEmail)
+    TextInputEditText edtEmail;
+    @BindView(R.id.textInputLayoutEmail)
+    TextInputLayout textInputLayoutEmail;
+    @BindView(R.id.imgNext)
+    AppCompatImageView imgNext;
+    @BindView(R.id.txtDontHave)
+    AppCompatTextView txtDontHave;
+    @BindView(R.id.txtSignup)
+    AppCompatTextView txtSignup;
+    @BindView(R.id.nestedScrollView)
+    NestedScrollView nestedScrollView;
+    Unbinder unbinder;
 
     public SignupOwnerFragment() {
         // Required empty public constructor
@@ -24,7 +76,136 @@ public class SignupOwnerFragment extends Fragment implements SignupOwnerControll
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup_owner, container, false);
+        View view = inflater.inflate(R.layout.fragment_signup_owner, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.resturantImageView)
+    public void onViewClicked() {
+        selectImage();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void selectImage() {
+        final CharSequence[] items = {"Take Photo", "Choose from Library",
+                "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(getActivity());
+                if (items[item].equals("Take Photo")) {
+                    userChoosenTask = "Take Photo";
+                    if (result)
+                        System.out.println("*******R*******take photo selected");
+                    cameraIntent();
+                } else if (items[item].equals("Choose from Library")) {
+                    userChoosenTask = "Choose from Library";
+                    if (result)
+                        System.out.println("*******R*******Gallery selected");
+                    galleryIntent();
+                } else if (items[item].equals("Cancel")) {
+                    System.out.println("*******R*******cancel selected");
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+
+    private void cameraIntent() {
+        System.out.println("*******R*******open camera");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    private void galleryIntent() {
+        System.out.println("*******R*******open gallery");
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        resturantImageView.setImageBitmap(bm);
+    }
+
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        resturantImageView.setImageBitmap(thumbnail);
+    }
+
+
+
+
+
 
 }
